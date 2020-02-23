9e2dae3c5328fcd17c6d988285dd6332c0164ab0
Laila Campbell <lailacampbell96@gmail.com>
2020-02-23 16:10:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,5 @@
 require prime
 # Add  code here!
-def prime?(num)
-    flag = true
-    for x in 2..num-1
-        if num % x == 0
-            flag = false
-        end
-    end
-    return flag
+def prime?(integer)
+    
 end
