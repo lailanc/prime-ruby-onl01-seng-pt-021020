544b3e7c363bd1ac1b82a5dd717ccdff7dea0171
Laila Campbell <lailacampbell96@gmail.com>
2020-02-23 16:12:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,9 @@
 require prime
 # Add  code here!
-def prime?(integer)
-
-end
+def prime?(number_to_test)
+  return false if number_to_test < 2
+  (2..Math.sqrt(number_to_test)).each do |num|
+    return false if number_to_test % num == 0
+  end
+  true
+end 
